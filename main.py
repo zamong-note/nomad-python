eed547d9991f076d7936ef02de234ca5e2c39117
zamong-note <tamongnote@naver.com>
2026-05-07 21:49:10 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from random import randint
+
+print("Welcome to Python Casino")
+
+playing = True
+
+pc_choice = randint(1, 50)
+
+while playing:
+	user_choice = int(input("Choose number:"))
+
+	if user_choice == pc_choice:
+		print("YOU WON!")
+		playing = False
+	elif user_choice > pc_choice:
+		print("Lower!")
+	elif user_choice < pc_choice:
+		print("Higher!")
