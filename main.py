aa451f6954fbfceb6bda81e13f5a795a9a04212a
zamong-note <tamongnote@naver.com>
2026-05-09 22:22:16 +0900

    #7.2 Render Template

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("JobScrapper") # 앱을 실행함
 
 @app.route("/")	# 사람들이 올 때 route할것. 유저가 방문했을 때 이 함수를 호출한다. 반드시 decorating 되어 있어야 동작한다. 항상 같이 있어야 함.   @는 decorator 이다. 
 def home():
-    return 'hey there!'
+    return render_template("home.html", name="nico")
+
+@app.route("/hello")
+def hello():
+    return 'hello you!'
 
 
 app.run("0.0.0.0")    # Replit에서 하지 않고 로컬에서 해서 좀 다르다..
