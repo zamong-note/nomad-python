ae50cb3f77a159aecf7c0562133cef98051c715b
zamong-note <tamongnote@naver.com>
2026-05-10 10:38:36 +0900

    #7.5 Arguments

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.wanted import extract_wandted_jobs
 
-app = Flask("JobScrapper") # 앱을 실행함
+app = Flask("JobScrapper") 
 
-@app.route("/")	# 사람들이 올 때 route할것. 유저가 방문했을 때 이 함수를 호출한다. 반드시 decorating 되어 있어야 동작한다. 항상 같이 있어야 함.   @는 decorator 이다. 
+@app.route("/")	
 def home():
     return render_template("home.html", name="nico")
 
 @app.route("/search")
-def hello():
-    return render_template("search.html")
+def search():
+    keyword = request.args.get("keyword")
+    wanted = extract_wandted_jobs(keyword)
+    return render_template("search.html", keyword=keyword, wanted=wanted)
 
 
 
-app.run("0.0.0.0")    # Replit에서 하지 않고 로컬에서 해서 좀 다르다..
+app.run("0.0.0.0")    
