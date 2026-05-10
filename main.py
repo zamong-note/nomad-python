7ce9d0f80ccf01187f8a54fd8e2398b3360a3c72
zamong-note <tamongnote@naver.com>
2026-05-10 18:21:09 +0900

    #7.8 Cache

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from extractors.wanted import extract_wanted_jobs
 
 app = Flask("JobScrapper") 
 
+db = {}
+
 @app.route("/")	
 def home():
     return render_template("home.html", name="nico")
@@ -10,7 +12,11 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    wanted_jobs = extract_wanted_jobs(keyword)
+    if keyword in db:
+        wanted_jobs = db[keyword]
+    else:
+        wanted_jobs = extract_wanted_jobs(keyword)
+        db[keyword] = wanted_jobs
     return render_template("search.html", keyword=keyword, wanted_jobs=wanted_jobs)
 
 
